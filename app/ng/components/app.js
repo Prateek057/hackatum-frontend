ec0e0d5060c5154ad57d983d9e668ac71cdd7d96
Akash Manjunath <akash.manjunath@gmail.com>
2016-06-04 13:16:54 +0200

    modules added in bower.json and app.js

--- app/ng/components/app.js
+++ app/ng/components/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', ['ui.router','ui.bootstrap','myApp.movies','myApp.dashboard','templates', 'ncy-angular-breadcrumb', 'ngMaterial', 'ngMessages'])
+angular.module('myApp', ['ui.router','ui.bootstrap','chart.js','myApp.movies','myApp.dashboard','templates', 'ncy-angular-breadcrumb', 'ngMaterial', 'ngMessages'])
 
     .config(function($stateProvider, $urlRouterProvider, $mdIconProvider, $resourceProvider, $httpProvider, $breadcrumbProvider) {
 
