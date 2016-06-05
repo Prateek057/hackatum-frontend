ee224ad1ec13da57b09e14e6bdc625b038388c50
Akash Manjunath <akash.manjunath@gmail.com>
2016-06-05 12:58:31 +0200

    Dashboard split into components using directives

---
+++ app/ng/components/success-ratio/success-ratio-directive.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Akash on 6/5/2016.
+ */
+'use strict';
+
+angular.module('myApp.dashboard')
+
+    .directive('mvSuccessRatio', function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'components/success-ratio/success-ratio.html'
+        };
+    });
