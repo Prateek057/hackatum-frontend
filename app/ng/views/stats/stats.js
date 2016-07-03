fd6c9b5b23d015f058a063aa73b01373431dc77f
Akash Manjunath <akash.manjunath@gmail.com>
2016-07-03 14:29:01 +0200

    admin charts bug fix

--- app/ng/views/stats/stats.js
+++ app/ng/views/stats/stats.js
@@ -40,65 +40,67 @@ angular.module('myApp.stats')
             var historylist = response[2].data;
             var historyresultlist = [];
             for(var ctr=0;ctr<historylist.length;ctr++){
-                var result = [];
-                result["analysisResult"] = historylist[ctr].analysisResult;
-                result["resultRating"] = historylist[ctr].resultRating;
-                historyresultlist.push(result);
+                if(historylist[ctr].resultRating){
+                    var analysisResultList = historylist[ctr].analysisResult;
+                    for(var ictr=0;ictr<analysisResultList.length;ictr++) {
+                        var result = [];
+                        result["analysisResult"] = analysisResultList[ictr];
+                        result["resultRating"] = historylist[ctr].resultRating;
+                        historyresultlist.push(result);
+                    }
+                }
             }
 
             //// CREATE CHART DATA FOR PATTERNS
 
-            var patternlabels = []
+            var patternlabels = [];
             $scope.patternvalues = [];
             for(var ctr=0;ctr<patternlist.length;ctr++){
                 patternlabels.push(patternlist[ctr].patternName);
                 //here iterate over the history list and create values
-                var count=0.0;
-                var sum=0.0;
+                var sum=0;
+                var count=0;
                 for(var ictr=0;ictr<historyresultlist.length;ictr++){
-                    if(patternlist[ctr].patternName==historyresultlist[ictr].analysisResult){
-                        sum+=historylist[ctr].resultRating;
+                    if(patternlist[ctr].patternName===historyresultlist[ictr].analysisResult){
+                        sum+=historyresultlist[ictr].resultRating;
                         count++;
                     }
                 }
-                if(sum==0.0){
+                if(sum==0){
                     $scope.patternvalues.push(0);
                 }
                 else {
                     var avg = sum/count;
-                    console.log(avg);
                     $scope.patternvalues.push(avg);
                 }
             }
             $scope.patterndata = [
                 $scope.patternvalues
-            ]
+            ];
             $scope.patternlabels = patternlabels;
-            console.log($scope.patternlabels);
-            console.log($scope.patternvalues);
 
             //// CREATE CHART DATA FOR ANTI-PATTERNS
 
-            var antipatternlabels = []
+            var antipatternlabels = [];
             $scope.antipatternvalues = [];
+
             for(var ctr=0;ctr<antipatternlist.length;ctr++){
                 antipatternlabels.push(antipatternlist[ctr].apname);
                 //here iterate over the history list and create values
-                var count=0.0;
-                var sum=0.0;
+                var count=0;
+                var sum=0;
                 for(var ictr=0;ictr<historyresultlist.length;ictr++){
-                    if(antipatternlist[ctr].apname==historyresultlist[ictr].analysisResult){
-                        sum+=historylist[ctr].resultRating;
+                    if(antipatternlist[ctr].apname===historyresultlist[ictr].analysisResult){
+                        sum+=historyresultlist[ictr].resultRating;
                         count++;
                     }
                 }
-                if(sum==0.0){
+                if(sum==0){
                     $scope.antipatternvalues.push(0);
                 }
                 else {
-                    var avg = 0.0;
+                    var avg = 0;
                     avg = sum/count;
-                    console.log(avg);
                     $scope.antipatternvalues.push(avg);
                 }
             }
@@ -106,14 +108,10 @@ angular.module('myApp.stats')
                 $scope.antipatternvalues
             ]
             $scope.antipatternlabels = antipatternlabels;
-            console.log($scope.antipatternlabels);
-            console.log($scope.antipatternvalues);
+
 
         });
 
-        $scope.onClick = function (points, evt) {
-            console.log(points, evt);
-        };
         $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
 
         $scope.height_chart = window.innerHeight*0.5;
